904a2541d31783eaa918c6498785c14ed59ae977
Marcus Perdue <marcusperduee@gmail.com>
2023-12-11 15:51:26 -0800

    added app.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Hero from './components/Hero'; 
+import About from './components/AboutMe';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Hero />} />  
+          <Route path="/portfolio" element={<Project />} />
+          <Route path="/about" element={<About />} />  
+          <Route path="/works" element={<Project />} />  
+          <Route path="/contact" element={<Contact />} />  
+          <Route path="/resume" component={Resume} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
