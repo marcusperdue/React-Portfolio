62e881693bf26f0dc3792f6f253fa1748223ba7c
Marcus Perdue <marcusperduee@gmail.com>
2023-12-16 14:25:31 -0800

    fixed linkin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
           <img src={githubIcon} alt="GitHub" className="w-6 h-6" />
           <span className="text-sm">GitHub</span>
         </a>
-        <a href="www.linkedin.com/in/marcus-perdue" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 ml-2">
+        <a href="https://www.linkedin.com/in/marcus-perdue/" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 ml-2">
           <img src={linkedinIcon} alt="LinkedIn" className="w-6 h-6" />
           <span className="text-sm">LinkedIn</span>
         </a>
